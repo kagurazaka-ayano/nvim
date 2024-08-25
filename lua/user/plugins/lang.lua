76ee2ced9bfa6f4ca2001a54e7b376fb1c46f01f
Ayano Kagurazaka <kagurazaka-ayano@outlook.com>
2024-08-25 13:33:17 -0400

    removed unnecessary config

--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -3,7 +3,6 @@ local lang = {}
 lang["nvim-java/nvim-java"] = {
 	ft = { "java", "gradle" },
 	init = function()
-		require("java").setup()
 		require("lspconfig").jdtls.setup({})
 	end,
 	config = require("configs.lang.nvim-java"),
