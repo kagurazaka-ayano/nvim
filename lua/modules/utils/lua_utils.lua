a5c463f32d7571dd6b284bb1d23d8e0d2bed003e
Ayano Kagurazaka <kagurazaka-ayano@outlook.com>
2025-01-20 12:06:13 -0500

    update

--- lua/modules/utils/lua_utils.lua
+++ lua/modules/utils/lua_utils.lua
@@ -64,28 +64,4 @@ function M.scandir(directory)
 	return t
 end
 
-function M.dirtree(dir)
-	assert(dir and dir ~= "", "Please pass directory parameter")
-	if string.sub(dir, -1) == "/" then
-		dir = string.sub(dir, 1, -2)
-	end
-
-	local function yieldtree(dir)
-		for entry in lfs.dir(dir) do
-			if entry ~= "." and entry ~= ".." then
-				entry = dir .. "/" .. entry
-				local attr = lfs.attributes(entry)
-				coroutine.yield(entry, attr)
-				if attr.mode == "directory" then
-					yieldtree(entry)
-				end
-			end
-		end
-	end
-
-	return coroutine.wrap(function()
-		yieldtree(dir)
-	end)
-end
-
 return M
