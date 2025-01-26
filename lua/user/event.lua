207a39dcefd4a277e1984b7f9a5050b334389198
Ayano Kagurazaka <kagurazaka-ayano@outlook.com>
2025-01-26 12:50:09 -0500

    no need

--- lua/user/event.lua
+++ lua/user/event.lua
@@ -1,8 +1,6 @@
 local definitions = {
 	-- Example
 	bufs = {
-		{ "BufReadPost", "/etc/nixos/*.conf", "setlocal filetype=nix" },
-		{ "BufReadPost", "/etc/nix/*.conf", "setlocal filetype=nix" },
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
 	},
 }
